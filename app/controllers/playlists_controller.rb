0ad4142b133e8eb1cbfd27728fded5bbbe9d041f
Muhammad Musah <muhammad@Muhammads-Machine.local>
2019-07-09 15:29:32 -0400

    Playlist CRUD complete

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -9,9 +9,28 @@ class PlaylistsController < ApplicationController
   end
 
   def new
+    @player = Player.new
   end
 
+  def create
+    player = Player.create(player_params)
+    redirect_to players_path
+  end 
+
   def edit
+    find_playlist
+  end 
+
+  def update
+    find_playlist
+    @playlist.update(playlist_params)
+    redirect_to playlist_path(@playlist)
+  end
+
+  def destroy
+    find_playlist    
+    @playlist.destroy
+    redirect_to playlists_path
   end
 
   private
@@ -19,4 +38,8 @@ class PlaylistsController < ApplicationController
   def find_playlist
     @playlist = Playlist.find(params[:id])
   end
+
+  def playlist_params
+    params.require(:playlist).permit(:name)
+  end 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @playlists = @user.playlists
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.create
+    redirect_to user_path
+  end 
 end
